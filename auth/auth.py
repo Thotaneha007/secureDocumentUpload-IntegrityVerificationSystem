6098e4028de1a858d4c13c305c267e9d1f557e73
Neha <144884109+Thotaneha007@users.noreply.github.com>
2026-01-28 15:03:53 +0530

    Add files via upload

---
+++ auth/auth.py
@@ -0,0 +1,9 @@
+import bcrypt
+
+def hash_password(password):
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def check_password(password, hashed):
+    if isinstance(hashed, str):
+        hashed = hashed.encode('utf-8')
+    return bcrypt.checkpw(password.encode('utf-8'), hashed)
